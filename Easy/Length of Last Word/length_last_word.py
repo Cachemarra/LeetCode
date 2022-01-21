a05fbb0f99aa28a4d8b21eacd7479bba600bf0c9
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2022-01-20 22:51:40 -0700

    Added the test cases.

--- Easy/Length of Last Word/length_last_word.py
+++ Easy/Length of Last Word/length_last_word.py
@@ -2,3 +2,24 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         list_of_words = s.split(' ')
         
+
+
+
+if __name__ == '__main__':
+
+    test_cases = ["Hello World", "   fly me   to   the moon  ", "luffy is still joyboy"]
+    results = [5, 4, 6]
+    count = 0
+
+    for index, item in enumerate(test_cases):
+        ans = Solution().lengthOfLastWord(item)
+        print(f'We get: {ans}, expected: {results[index]}')
+        if ans == results[index]:
+            print('Passed')
+            count += 1
+        else:
+            print('Failed')
+        print('\n')
+
+    print(f'We passed {count} out of {len(test_cases)} test cases')
+
