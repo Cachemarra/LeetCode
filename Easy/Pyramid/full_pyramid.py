ad62296db5a8001f55399652b03c6f8f0e40bbcf
Cachemarra <53624391+Cachemarra@users.noreply.github.com>
2022-01-26 22:40:44 -0700

    finished full pyramid

---
+++ Easy/Pyramid/full_pyramid.py
@@ -0,0 +1,40 @@
+#%% Def function of the full pyramid script
+from re import A
+
+
+def pyramid(n: int) -> None:
+    """
+    Print a full pyramid based on the n number of rows.
+    :param: n: int. Number of rows of the pyramid.
+    :return: None.
+    """
+    # Iterate over the number of rows
+    row = 0
+    actual_row = ''
+
+    for i in range(n):
+        row += 1
+        if row != 1:            
+            actual_row = '*' * (2 * row-1)
+            actual_row = actual_row.center(n*2, ' ')
+
+            print(actual_row)
+
+        else:
+            print('*'.center(n*2, ' '))
+
+
+
+
+#%%
+if __name__ == '__main__':
+
+    for i in range(5):
+        print(f'Test case {i}')
+        pyramid(i+1)
+        print('\n')
+
+    
+
+
+# %%
